1b65d17b238a94c3e4c6e65accfcb29ce4d8a2a6
Robert Moss <rmoss92@gmail.com>
2020-10-01 15:33:07 -0700

    Section counters updates

--- src/section_counters.jl
+++ src/section_counters.jl
@@ -1,6 +1,6 @@
 html"""<style>
 pluto-notebook {
-  counter-reset: section;
+  counter-reset: section footnote footnote-title;
 }
 pluto-notebook h2 {
   counter-reset: subsection;
@@ -12,5 +12,25 @@ pluto-notebook h2:before {
 pluto-notebook h3:before {
   counter-increment: subsection;
   content: counter(section) "." counter(subsection) " ";
+}
+
+a.footnote {
+    font-weight: normal !important;
+    font-size: 0 !important;
+    vertical-align: super;
+}
+a.footnote::before {
+    counter-increment: footnote;
+    content: "" counter(footnote) "";
+    font-size: 10px;
+}
+
+.footnote-title {
+    font-size: 0 !important;
+}
+.footnote-title::before {
+    counter-increment: footnote-title !important;
+    content: "[" counter(footnote-title) "]" !important;
+    font-size: 0.75rem !important;
 }
 """
